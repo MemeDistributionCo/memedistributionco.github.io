5f0bd4626ed47cf4aace6041987fe93517eee009
Zach | xDest <destroyer7712@gmail.com>
2018-01-10 10:41:36 -0500

    Movement

--- userfiles.js
+++ userfiles.js
@@ -6,3 +6,14 @@ function createUserFromId() {
     
 }
 
+
+function userSearch() {
+    var userId = document.getElementById("searchbar").value;
+    var userStats = {};//get stats here :ll
+    
+}
+
+function MDCUser() {
+    this.stats = {};//Collection of stats
+    this.userId = 0;//User id
+}
